d23b0e4c75c4023f187184a62036b6ca4b148fa1
429272-21 <429272-21>
2019-12-19 11:29:31 +0300

    laba4

--- index.php
+++ index.php
@@ -7,3 +7,21 @@ include_once 'Alex/Linear.php';
 include_once 'Alex/Square.php';
 include_once 'Alex/SjedinException.php';
 include_once 'Alex/Log.php';
+
+$arr=array();
+
+$arr[] = readline("a= ");
+$arr[] = readline("b= ");
+$arr[] = readline("c= ");
+
+try {
+    $solver = new Alex\Square();
+	$roots = $solver->solve($arr[0], $arr[1], $arr[2]);
+
+    Alex\Log::log("roots: " . implode(" , ", $roots));
+   
+}catch(Alex\SjedinException $e) {
+
+    Alex\Log::log($e->getMessage());
+}
+Alex\Log::write();
