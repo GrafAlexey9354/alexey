99911cf96fb8b8f5157bb9df3d56d2fc1b9c0ad0
AlekceyCjedin <56478639+AlekceyCjedin@users.noreply.github.com>
2020-04-22 14:54:31 +0300

    laba 11

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once'core/EquationInterface.php';
+include_once'core/LogInterface.php';
+include_once'core/LogAbstract.php';
+include_once 'Alex/Linear.php';
+include_once 'Alex/Square.php';
+include_once 'Alex/SjedinException.php';
+include_once 'Alex/Log.php';
+
+$arr=array();
+
+Alex\Log::log('Version '.file_get_contents('./version'));
+
+$arr[] = readline("a= ");
+$arr[] = readline("b= ");
+$arr[] = readline("c= ");
+
+try {
+    $solver = new Alex\Square();
+	$roots = $solver->solve($arr[0], $arr[1], $arr[2]);
+
+    Alex\Log::log("roots: " . implode(" , ", $roots));
+   
+}catch(Alex\SjedinException $e) {
+
+    Alex\Log::log($e->getMessage());
+}
+Alex\Log::write();
